f1883588c765238862a66cd064c6605d9937d40f
Cecil <cecil.cao@mail.utoronto.ca>
2020-07-17 16:39:51 -0400

    Add program title + short description. Modify instantiation of a counter variable @ line 25.

--- HighLowGame/Source.cpp
+++ HighLowGame/Source.cpp
@@ -1,3 +1,9 @@
+/* High-Low Game
+A console game that where you guess a number between 1 to 100 inclusive.
+The program will tell you whether your guess is too high or too low if it's wrong.
+You will have multiple chances to input the correct answer.
+*/
+
 #include <iostream>
 #include <random>
 #include <ctime>
@@ -16,7 +22,7 @@ int main() {
 	int guess; // Stores the guess that the user inputs.
 
 	// GAME LOOP
-	for (int i = 0; i < MAX_GUESSES; i++) {
+	for (int i{ 0 }; i < MAX_GUESSES; i++) {
 
 		// Shows how many guesses you have left.
 		if (i < MAX_GUESSES - 1) {
